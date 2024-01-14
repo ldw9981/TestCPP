80ba8ef142795b82b9d3a351e93a8da6074fc41e
LeeDongWon <ldw9981@gmail.com>
2024-01-14 21:36:38 +0900

    update

---
+++ Source/TestCPP/Portal.cpp
@@ -0,0 +1,48 @@
+﻿#include "Portal.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+// 기본값 설정
+APortal::APortal()
+{
+	// 모든 프레임에서 Tick()을 호출하도록 이 actor을(를) 설정합니다. 필요하지 않은 경우 이 기능을 해제하여 성능을 향상시킬 수 있습니다.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// 컴포넌트생성
+	Collision = CreateDefaultSubobject<UBoxComponent>(TEXT("Collision"));
+	Collision->SetupAttachment(RootComponent);  // 최상위 루트로 연결
+
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	StaticMesh->SetupAttachment(Collision);   // 컬리전의 자식으로 연결
+	StaticMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);	// 바닥 메시 충돌은 사용안함.
+
+	//엔진이 등록된 포인터함수를 호출한다.
+	OnActorBeginOverlap.AddDynamic(this, &APortal::OnOverlap);
+}
+
+// 게임이 시작되거나 생성될 때 호출됩니다.
+void APortal::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void APortal::OnOverlap(AActor* OverlappedActor, AActor* OtherActor)
+{
+	// 현재 플레이어 캐릭터와 Overlap된 Actor가 같은지 확인한다.
+	AActor* result = (AActor*)UGameplayStatics::GetPlayerCharacter(this, 0);
+	if(OtherActor != result)
+	{
+		return;
+	}
+
+	// 경로가 어셋인지 확인하고 맞으면 레벨을 로드한다.
+	if(Level.ToSoftObjectPath().IsAsset())
+		UGameplayStatics::OpenLevelBySoftObjectPtr(this,Level);	
+}
+
+// 매 프레임마다 호출됨
+void APortal::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
---
+++ Source/TestCPP/Portal.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Portal.generated.h"
+
+UCLASS()
+class TESTCPP_API APortal : public AActor
+{
+	GENERATED_BODY()
+	
+public:
+	// 이 actor의 속성에 대한 기본값을 설정합니다.
+	APortal();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Portal, meta = (AllowPrivateAccess = "true"))
+	class UBoxComponent* Collision;			// 오버랩을 검출할 영역
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Portal, meta = (AllowPrivateAccess = "true"))
+	class UStaticMeshComponent* StaticMesh;  // 포탈의 바닥 표시
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Portal, meta = (AllowPrivateAccess = "true"))
+	TSoftObjectPtr<UWorld> Level;	// 에셋의 경로만 지정한다. (경로만 저장되고, 에셋이 로드되지는 않는다.)
+
+protected:
+	// 게임이 시작되거나 생성될 때 호출됩니다.
+
+	virtual void BeginPlay() override;
+
+	UFUNCTION()	// 델리게이트 이벤트 시스템을 사용하기 위해 선언
+	void OnOverlap(AActor* OverlappedActor, AActor* OtherActor );
+public:
+	// 매 프레임마다 호출됨
+	virtual void Tick(float DeltaTime) override;
+};
